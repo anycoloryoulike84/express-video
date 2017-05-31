493d8800848653a5590ecc67baa0443b963ffb18
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-05-30 19:25:47 -0700

    new modules, new files

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+
+
+
+app.get('/blocks', function(request, response){
+	var blocks = ["first","second","third"];
+	response.json(blocks);
+});
+
+var port = 8080;
+
+app.listen(port, function () {
+  console.log('Example app listening on port: ' + port);
+});
+
+// curl -i http://localhost:8000/
+
+
---
+++ public/client.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+	$(function(){
+
+		$.get("/blocks", appendToList);
+
+		function appendToList(blocks) {
+			var list = [];
+			for (var i in blocks) {
+				list.push($("<li>", {text:blocks[i]}));
+			}
+			$(".block-list").append(list);
+		}
+
+
+		console.log("client.js works");
+		console.dir(blocks)
+
+
+
+	});
+
+

151d0e07a6cd773333e1e171870ae8618cb8c16c
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-05-31 14:35:56 -0700

    dynamic route updates

--- app.js
+++ app.js
@@ -15,21 +15,51 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 		"Moving": "moving fast",
 		"Rotating": "rotating fast"
 	};
+	var locations = {
+		"Fixed": "fastened securely",
+		"Moving": "moving fast",
+		"Rotating": "rotating fast"
+	};
+
+
+
+app.param('name', function(request, response, next){
+	
+		var name = request.params.name;
+		var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+		request.blockName = block;
+		next();
+});
+
 
 app.get('/blocks/:name', function(request, response){
 	
-		var name = request.params.name;
-		var block = name[0].toUpperCase() + name.slice(1).toLowerCase();	
-		var description = blocks[block];
+
+		var description = blocks[request.blockName];
 
 		if (description) {
-			response.json(description);
+			response.json(Object.keys(description));
 
 		} else{
 			response.status(404).json("no description for: " + request.params.name);
 		};
-		
 });
+
+
+app.get('/locations/:name', function(request, response){
+	
+
+		var locations = blocks[request.blockName];
+
+		if (locations) {
+			response.json(Object.keys(locations));
+
+		} else{
+			response.status(404).json("no description for: " + request.params.name);
+		};
+});
+
+
 
 var port = 8080;
 	app.listen(port, function () {
@@ -37,5 +67,6 @@ var port = 8080;
 });
 
 // curl -i http://localhost:8080/blocks?limit=1
+// curl -i http://localhost:8080/blocks/fixed
 
 

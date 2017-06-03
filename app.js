c6dc20897b8e81732d1e523ea2e75529a8e7cdec
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-02 18:14:46 -0700

    new form that appends new block. BUT --  NEW BUG: the name directory is issuing a 404 for the name of the description

--- app.js
+++ app.js
@@ -4,14 +4,16 @@
 var express = require('express');
 var app = express();
 var loadingTime = require("./loading_time");
+var bodyParser = require("body-parser");
+var parseUrlencoded = bodyParser.urlencoded({extended:false});
+
 
 app.use(loadingTime);
 app.use(express.static("public"));
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 
-	
+
 	var blocks = {
 		"Fixed": "fastened securely",
 		"Moving": "moving fast",
@@ -23,6 +25,13 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 		"Newestlocation": "rotating fast"
 	};
 
+
+
+app.post("/blocks", parseUrlencoded, function(request,response){
+	var newBlock = request.body;
+	blocks[newBlock.name] = newBlock.description;
+	response.status(201).json(newBlock.name);
+});
 
 
 app.get('/blocks', function(request, response){
--- public/client.js
+++ public/client.js
@@ -10,17 +10,6 @@
 
 
 
-		function appendToList(blocks) {
-			var list = [];
-			var content, block;
-			for (var i in blocks) {
-				block = blocks[i];
-				content = '<a href="/blocks/' + block +'">'+block+'</a>';
-				list.push($("<li>", { html:content }));
-			}
-			$(".block-list").append(list);
-		}
-
 
 		$("form").on("submit",function(e){
 			e.preventDefault();
@@ -36,6 +25,19 @@
 				form.trigger('reset');
 			});
 		});
+
+
+		function appendToList(blocks) {
+			var list = [];
+			var content, block;
+			for (var i in blocks) {
+				block = blocks[i];
+				content = '<a href="/blocks/' + block +'">' + block + '</a>';
+				list.push($("<li>", { html:content }));
+			}
+			$(".block-list").append(list);
+		}
+
 
 
 		
